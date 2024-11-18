0ad4b20b8b50890ce42ea80b86d7e61eb17bca36
Haku <polina231003@mail.ru>
2024-11-18 15:13:31 +0300

    Singleton example

--- src/ru/neoflex/edu/homework1/Main.java
+++ src/ru/neoflex/edu/homework1/Main.java
@@ -1,9 +1,15 @@
 package ru.neoflex.edu.homework1;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import ru.neoflex.edu.homework1.singleton.LoggerSingleton;
+import ru.neoflex.edu.homework1.singleton.SomeClass;
 
 public class Main {
     public static void main(String[] args) {
+        //singleton
+        SomeClass someClass = new SomeClass();
+        LoggerSingleton loggerSingleton = LoggerSingleton.getInstance();
+        loggerSingleton.classLog(someClass, "start doing something");
+        someClass.doSomething();
+        loggerSingleton.classLog(someClass, "something done");
     }
 }
---
+++ src/ru/neoflex/edu/homework1/singleton/SomeClass.java
@@ -0,0 +1,7 @@
+package ru.neoflex.edu.homework1.singleton;
+
+public class SomeClass {
+    public void doSomething() {
+        System.out.println("doing something");
+    }
+}
